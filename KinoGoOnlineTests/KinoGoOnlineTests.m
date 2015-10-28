ab57198c28ce977aedc6cafc65d1a9aad55d65b0
Ronan O Ciosoig <ronanoc@icloud.com>
2015-10-28 15:11:26 +0100

    Online services validated.

--- KinoGoOnlineTests/KinoGoOnlineTests.m
+++ KinoGoOnlineTests/KinoGoOnlineTests.m
@@ -34,14 +34,97 @@
     [super tearDown];
 }
 
-/*
- 
- extern NSString *const kKinofinderWebserviceRoot;
- extern NSString *const kKinofinderWebserviceUrlCharts;
- */
+- (void)testTheAPIReturnsValidDataForCharts {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    NSString *requestPath = [NSString stringWithFormat:@"%@%@", kKinofinderWebserviceRoot,kKinofinderWebserviceUrlCharts];
+    SONLog(@"Request path: %@", requestPath);
+    url = [NSURL URLWithString:requestPath];
+    XCTAssert(manager,@"Network manager must not be nil.");
+    request = [manager requestForURL:url];
+    XCTAssert(request,@"Request object must be valid.");
+    // Given
+    XCTestExpectation *validServerResponseExpectation = [self expectationWithDescription:@"valid response for HTTP request."];
+    
+    // When
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse*)response;
+        NSLog(@"Response: %@", [response description]);
+        if (httpResponse.statusCode >= kHTTP_RESPONSE_CODE_OK && httpResponse.statusCode < 300) {
+            XCTAssert(YES,@"HTTP response should be greater than or equal to 200 and less than 300");
+            
+            NSError *jsonError;
+            id jsonObject =
+            [NSJSONSerialization JSONObjectWithData:data
+                                            options:NSJSONReadingAllowFragments
+                                              error:&jsonError];
+            if (!jsonError) {
+                XCTAssertTrue([jsonObject isKindOfClass:[NSArray class]], @"The JSON object response should be an array.");
+                NSArray *jsonResponse = (NSArray*)jsonObject;
+                XCTAssertNotNil(jsonResponse, @"Response should have valid JSON data.");
+                [validServerResponseExpectation fulfill];
+            } else {
+                XCTFail(@"Error parsing the response. %@",[jsonError localizedDescription]);
+            }
+            
+        } else {
+            XCTFail(@"Error loading the response. %d", (int) httpResponse.statusCode);
+        }
+    }];
+    [task resume];
+    
+    // Then
+    [self waitForExpectationsWithTimeout:5 handler:^(NSError *error) {
+        // nothing to do here
+    }];
+}
 
+- (void)testTheAPIReturnsValidDataForRunning {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    NSString *requestPath = [NSString stringWithFormat:@"%@%@", kKinofinderWebserviceRoot,kKinofinderWebserviceUrlCharts];
+    SONLog(@"Request path: %@", requestPath);
+    url = [NSURL URLWithString:requestPath];
+    XCTAssert(manager,@"Network manager must not be nil.");
+    request = [manager requestForURL:url];
+    XCTAssert(request,@"Request object must be valid.");
+    // Given
+    XCTestExpectation *validServerResponseExpectation = [self expectationWithDescription:@"valid response for HTTP request."];
+    
+    // When
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse*)response;
+        NSLog(@"Response: %@", [response description]);
+        if (httpResponse.statusCode >= kHTTP_RESPONSE_CODE_OK && httpResponse.statusCode < 300) {
+            XCTAssert(YES,@"HTTP response should be greater than or equal to 200 and less than 300");
+            
+            NSError *jsonError;
+            id jsonObject =
+            [NSJSONSerialization JSONObjectWithData:data
+                                            options:NSJSONReadingAllowFragments
+                                              error:&jsonError];
+            if (!jsonError) {
+                XCTAssertTrue([jsonObject isKindOfClass:[NSArray class]], @"The JSON object response should be an array.");
+                NSArray *jsonResponse = (NSArray*)jsonObject;
+                XCTAssertNotNil(jsonResponse, @"Response should have valid JSON data.");
+                [validServerResponseExpectation fulfill];
+            } else {
+                XCTFail(@"Error parsing the response. %@",[jsonError localizedDescription]);
+            }
+            
+        } else {
+            XCTFail(@"Error loading the response. %d", (int) httpResponse.statusCode);
+        }
+    }];
+    [task resume];
+    
+    // Then
+    [self waitForExpectationsWithTimeout:5 handler:^(NSError *error) {
+        // nothing to do here
+    }];
+}
 
-- (void)testTheAPIReturnsValidData {
+- (void)testTheAPIReturnsValidDataForPreviews {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
     NSString *requestPath = [NSString stringWithFormat:@"%@%@", kKinofinderWebserviceRoot,kKinofinderWebserviceUrlCharts];
