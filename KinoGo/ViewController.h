ba31003e9c1dc5c42d2ce2d5bbb7bd4fc6ce11b5
Ronan O Ciosoig <ronanoc@icloud.com>
2015-10-28 12:03:08 +0100

    Initial Commit

---
+++ KinoGo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KinoGo
+//
+//  Created by Ronan on 28/10/2015.
+//  Copyright © 2015 Sonomos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
